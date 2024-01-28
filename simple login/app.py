fba830a3e3b431135d3f7272a93b07f7b04b08fe
Shrutika Shaw <shrutika.shaw2015@gmail.com>
2024-01-28 20:08:41 +0530

    simple login

---
+++ simple login/app.py
@@ -0,0 +1,14 @@
+print("Create your account now")
+username=input("Enter Username: ")
+password=input("Enter Password: ")
+
+print("Account Created successfully")
+
+print("Login now")
+username1=input("Enter Username: ")
+password1=input("Enter Password: ")
+
+if username==username1 and password==password1:
+    print("Logged in Successfully")
+else:
+    print("Invalid Credentials")
